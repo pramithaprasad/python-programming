aa84be6785a61cd8fad9b043f7238175344694e9
pramithaprasad <37370142+pramithaprasad@users.noreply.github.com>
2018-03-18 14:13:07 +0530

    Delete vowel.py

--- Beginner Level/vowel.py
+++
@@ -1,6 +0,0 @@
-ch=input("enter the value")
-if((ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u')or(ch=='A'or  ch=='E'or ch=='I' or ch=='O'or ch=='U')):
-     print("vowel")
-else:
-      print("consonant")
- 
