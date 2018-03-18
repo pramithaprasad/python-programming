1c779b135f0ba67928f0319690e679af880ec9c3
pramithaprasad <37370142+pramithaprasad@users.noreply.github.com>
2018-03-18 12:18:01 +0530

    Update positive or negative.py

--- Beginner Level/positive or negative.py
+++ Beginner Level/positive or negative.py
@@ -1,7 +1,8 @@
-a=int(input(number))
-if(a>0):
- print("a is positive")
+a=int(input("number"))
+if(a>1 and a<=100000):
+   print("a is positive")
 elif(a<0):
- print("a is negative")
+   print("a is negative")
 else:
- print("a is zero")
+   print("a is zero")
+ 
