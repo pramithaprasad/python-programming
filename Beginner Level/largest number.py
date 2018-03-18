792882022386ee54a120e54cab8a96912d229546
pramithaprasad <37370142+pramithaprasad@users.noreply.github.com>
2018-03-18 14:29:36 +0530

    Create largest number.py

---
+++ Beginner Level/largest number.py
@@ -0,0 +1,9 @@
+a1=int(input("number 1"))
+a2=int(input("number 2"))
+a3=int(input("number 3"))
+if((a1>a2) and (a1>a3)):
+ print(" largest number is" +str(a1))
+elif((a2>a1) and (a2>a3)):
+ print(" largest number is" +str(a2))
+else:
+ print(" largest number is" +str(a3))
