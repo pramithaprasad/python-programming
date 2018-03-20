4e38c91cafe4bb9a4f664711997355b8adcd8c11
pramithaprasad <37370142+pramithaprasad@users.noreply.github.com>
2018-03-20 14:36:00 +0530

    Update odd numbers.py

--- Beginner Level/odd numbers.py
+++ Beginner Level/odd numbers.py
@@ -1,6 +1,6 @@
-n=int(input("number 1"))
-s=int(input("number 2"))
-for i in range(n,s-1):
+q=int(input("number 1"))
+w=int(input("number 2"))
+for i in range(q,w-1):
   if(i%2==0):
     print(i+1)
   
