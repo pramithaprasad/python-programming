1341a22b84783a68d87b10af51aece470abc227d
pramithaprasad <37370142+pramithaprasad@users.noreply.github.com>
2018-04-28 10:15:15 +0530

    Create fibonaccio series.py

---
+++ Beginner Level/fibonaccio series.py
@@ -0,0 +1,12 @@
+n=int(input("enter the number"))
+x=0
+y=1 
+for a in range(n):
+    if(a<=1):
+        next=a 
+    else:
+        next=x+y 
+        x=y
+        y=next
+    a=a+1
+    print(next)
